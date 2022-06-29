37b3f5d395b1436ed44afa7a89df6b802f3d8e03
ayobamibolt <91104931+ayobamibolt@users.noreply.github.com>
2022-06-29 13:12:54 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region     = "${var.region}" 
+}
+
+#Create s3 bucket
+resource "aws_s3_bucket" "terraform_state_s3" {
+  bucket = "nice-devops-interview" 
+}
+
+#uploading python zip to lambda funtion
+data "archive_file" "zip_the_python_code" {
+type        = "zip"
+source_dir  = "${path.module}/python/"
+output_path = "${path.module}/python/file-parser.zip"
+
+}
+
+
+resource "aws_lambda_function" "lambda-nice-devops-interview" {
+filename                       = "${path.module}/python/file-parser.zip"
+function_name                  = "Spacelift_Test_Lambda_Function"
+role                           = arn:aws:iam::557414474363:role/lambda-execution-devops-interview
+handler                        = "index.lambda_handler"
+runtime                        = "python3.8"
+
+}
+
+
+
