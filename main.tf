7951207477fb5a446677418f7f4c12f4f8e23c85
Ayobami Alabi <ayobamialabi@Ayobamis-MacBook-Pro.local>
2022-07-01 01:55:32 -0400

    terraform changes

--- main.tf
+++ main.tf
@@ -24,36 +24,3 @@ runtime                        = "python3.8"
 resource "aws_s3_bucket" "terraform_state_s3" {
   bucket = "nice-devops-interview" 
 }
-data "s3_policy-document" "example"{
-
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-            "Sid": "ListBucket",
-            "Effect": "Allow",
-            "Principal": {
-                "AWS": "arn:aws:iam::557414474363:role/lambda-execution-devops-intervie"
-            },
-            "Action": "s3:ListBucket",
-            "Resource": "arn:aws:s3:::bucket"
-        },
-        {
-            "Sid": "GetObjects",
-            "Effect": "Allow",
-            "Principal": {
-                "AWS": "arn:aws:iam::557414474363:role/lambda-execution-devops-intervie"
-            },
-            "Action": "s3:GetObject",
-            "Resource": "arn:aws:s3:::bucket/*"
-        }
-      
-    ]
-  
-}
-
-resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
-  bucket = aws_s3_bucket.terraform_state_s3.id
-  policy = data.s3_policy-document.example.json
-
-}
-
