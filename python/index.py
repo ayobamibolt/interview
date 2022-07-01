2fe470fc536e0b9ef98d735b741effdc6f23883f
Ayobami Alabi <ayobamialabi@Ayobamis-MacBook-Pro.local>
2022-07-01 02:04:28 -0400

    terraform changes

--- python/index.py
+++ python/index.py
@@ -6,7 +6,7 @@ S3_BUCKET = 'nice-devops-interview'
 def lambda_handler(event, context):
   object_key = "parseme"  # replace object key
   file_content = (s3_client.Object(
-      Bucket=S3_BUCKET, Key=object_key)).get()["Body"].read()
+      bucket_name=S3_BUCKET, key=object_key)).get()["Body"].read()
   print(file_content)
   final_string = ''.join((x for x in (file_content.replace("00", " ")) if not x.isdigit()))
   return final_string
