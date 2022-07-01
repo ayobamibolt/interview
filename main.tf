48817a807db090b625299d1e597496674f5aeb45
Ayobami Alabi <ayobamialabi@Ayobamis-MacBook-Pro.local>
2022-07-01 00:10:33 -0400

    terraform changes

--- main.tf
+++ main.tf
@@ -6,16 +6,17 @@ provider "aws" {
 data "archive_file" "zip_the_python_code" {
 type        = "zip"
 source_dir  = "${path.module}/python/"
-output_path = "file-parser.zip"
+output_path = "${path.module}/python/file-parser.zip"
 
 }
 
 
 resource "aws_lambda_function" "lambdaTest" {
-filename                       = "file-parser.zip"
+filename                       = "${path.module}/python/file-parser.zip"
 function_name                  = "lambda-nice-devops-interview"
 source_code_hash               = data.archive_file.zip_the_python_code.output_base64sha256
 role                           = "arn:aws:iam::557414474363:role/lambda-execution-devops-interview"
+handler                        = "file-parser.zip.lambda_handler"
 runtime                        = "python3.8"
 
 }
