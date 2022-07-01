4f251adad5c9c688a6a3fd1e2bbb02584b4857df
Ayobami Alabi <ayobamialabi@Ayobamis-MacBook-Pro.local>
2022-07-01 15:40:49 -0400

    printing type

--- python/index.py
+++ python/index.py
@@ -1,12 +1,12 @@
 import boto3
 
-s3_client = boto3.resource("s3")
+s3_client = boto3.client("s3")
 S3_BUCKET = 'nice-devops-interview'
 
 def lambda_handler(event, context):
-  object_key = "parse_me.txt"  # replace object key
-  file_content = (s3_client.Object(
-      bucket_name=S3_BUCKET, key=object_key)).get()["Body"].read()
+  object_key = "parse_me.txt"
+  file_content = s3_client.get_object(
+      bucket_name=S3_BUCKET, key=object_key)["Body"].read().decode('utf-8')
   print(type(file_content))
   final_string = ''.join((x for x in (file_content.replace("00", " ")) if not x.isdigit()))
   return final_string
